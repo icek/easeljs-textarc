acb5a4e41c2d6d74dd3eae001f2b89b64cbcc863
Grzegorz Michlicki <grzegorz@michlicki.pl>
2014-06-04 17:46:53 +0200

    Rewritten drawing algoritm. EaselJS 0.7.1 compatibility.

--- easeljs-textarc.js
+++ easeljs-textarc.js
@@ -22,7 +22,7 @@ this.createjs=this.createjs||{};
     // define a new TextArc class that extends Text and drawing arc text.
     var TextArc = function (text, font, color, radius) {
         this.initialize(text, font, color, radius);
-    }
+    };
 
     TextArc.prototype = new createjs.Text(); // extend Text.
 
@@ -33,28 +33,32 @@ this.createjs=this.createjs||{};
     TextArc.prototype.initialize = function (text, font, color, radius) {
         this.Text_initialize(text, font, color);
         this.radius = radius;
-        this.textBaseline = "center";
-    }
+        this.textBaseline = "middle";
+    };
 
     // use the same approach with draw:
     TextArc.prototype.Text_drawTextLine = TextArc.prototype._drawTextLine;
 
     //Override _drawTextLine method
     TextArc.prototype._drawTextLine = function (ctx, text, y) {
-        var wordWidth = ctx.measureText(text).width;
-        var angle = 2 * Math.asin(wordWidth / ( 2 * this.radius ));
+        var textWidth = ctx.measureText(text).width,
+            angle = textWidth / this.radius;
         ctx.save();
-        ctx.rotate(-1 * angle / 2);
-        ctx.rotate(-1 * (angle / text.length) / 2);
+        ctx.rotate(-angle / 2);
+
         for (var i = 0; i < text.length; i++) {
-            ctx.rotate(angle / text.length);
+            var letterWidth = ctx.measureText(text[i]).width,
+                ang = angle * letterWidth / textWidth / 2;
+            ctx.rotate(ang);
             ctx.save();
-            ctx.translate(0, -1 * this.radius);
+            ctx.translate(-letterWidth/2, -this.radius);
             this.Text_drawTextLine(ctx, text[i], y);
             ctx.restore();
+            ctx.rotate(ang);
+
         }
         ctx.restore();
-    }
+    };
 
     createjs.TextArc = TextArc;
 }());
