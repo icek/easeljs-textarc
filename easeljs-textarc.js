11f8275c32d3ebeb70aef1845524bbbdecd074d1
Grzegorz Michlicki <grzegorz@michlicki.pl>
2014-06-09 17:30:50 +0200

    Ability to write text upside down

--- easeljs-textarc.js
+++ easeljs-textarc.js
@@ -20,38 +20,47 @@ this.createjs=this.createjs||{};
 (function () {
 
     // define a new TextArc class that extends Text and drawing arc text.
-    var TextArc = function (text, font, color, radius) {
-        this.initialize(text, font, color, radius);
+    var TextArc = function (text, font, color, radius, inverted) {
+        this.initialize(text, font, color, radius, inverted);
     };
 
-    TextArc.prototype = new createjs.Text(); // extend Text.
+    var p = TextArc.prototype = new createjs.Text(); // extend Text.
 
     // save off initialize method from Text so we can call it from our own:
-    TextArc.prototype.Text_initialize = TextArc.prototype.initialize;
+    p.Text_initialize = TextArc.prototype.initialize;
 
     // overwrite Text's initialize method with our own:
-    TextArc.prototype.initialize = function (text, font, color, radius) {
+    p.initialize = function (text, font, color, radius, inverted) {
         this.Text_initialize(text, font, color);
         this.radius = radius;
+        this.inverted = inverted || false;
         this.textBaseline = "middle";
     };
 
     // use the same approach with draw:
-    TextArc.prototype.Text_drawTextLine = TextArc.prototype._drawTextLine;
+    p.Text_drawTextLine = TextArc.prototype._drawTextLine;
 
     //Override _drawTextLine method
-    TextArc.prototype._drawTextLine = function (ctx, text, y) {
+    p._drawTextLine = function (ctx, text, y) {
         var textWidth = ctx.measureText(text).width,
             angle = textWidth / this.radius;
+        if(this.inverted) angle*=-1;
+
         ctx.save();
         ctx.rotate(-angle / 2);
 
         for (var i = 0; i < text.length; i++) {
             var letterWidth = ctx.measureText(text[i]).width,
                 ang = angle * letterWidth / textWidth / 2;
+
             ctx.rotate(ang);
             ctx.save();
-            ctx.translate(-letterWidth/2, -this.radius);
+            if(this.inverted){
+                ctx.translate(letterWidth/2, -this.radius);
+                ctx.rotate(Math.PI)
+            }else{
+                ctx.translate(-letterWidth/2, -this.radius);
+            }
             this.Text_drawTextLine(ctx, text[i], y);
             ctx.restore();
             ctx.rotate(ang);
